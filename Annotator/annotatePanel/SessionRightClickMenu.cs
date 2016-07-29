1ea9561e18f415c87ebcb16d447db79714c94146
Tuan <tuandn@brandeis.edu>
2016-07-29 11:00:28 -0400

    Add untracked files

---
+++ Annotator/annotatePanel/SessionRightClickMenu.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Annotator
+{
+    public partial class SessionRightClickMenu
+    {
+        LeftMostPanel leftMostPanel;
+
+        public SessionRightClickMenu(IContainer container, LeftMostPanel leftMostPanel) : base(container)
+        {
+            InitializeComponent();
+            this.leftMostPanel = leftMostPanel;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void editSessionMenuItem_Click(object sender, EventArgs e)
+        {
+            //Check selected node:
+            Session chosenSession = null;
+            if (treeView.SelectedNode.Text[0] == '*')
+            {
+                chosenSession = selectedProject.getSession(treeView.SelectedNode.Text.Substring(1));
+            }
+            else
+            {
+                chosenSession = selectedProject.getSession(treeView.SelectedNode.Text);
+            }
+
+            // Save current session if it is edited
+            if (currentSession != null && chosenSession != null && currentSession.sessionName != chosenSession.sessionName)
+            {
+                if (currentSession.getEdited())
+                {
+                    currentSession.setEdited(false);
+                    treeView.BeginUpdate();
+                    currentSessionNode.Text = currentSessionNode.Text.Substring(1);
+                    treeView.EndUpdate();
+
+                    var result = MessageBox.Show(("Session " + currentSession.sessionName + " currently editing, Do you want to save this session?"), "Save session", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        cleanCurrentSession();
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        cleanSessionUI();
+                    }
+                }
+            }
+
+            // Set current session = chosen session
+            if (chosenSession != null && !chosenSession.getEdited())
+            {
+                chosenSession.setEdited(true);
+                currentSessionNode = treeView.SelectedNode;
+                currentSession = chosenSession;
+                currentSession.loadIfNotLoaded();
+                currentSessionNode.Text = "*" + currentSessionNode.Text;
+
+                frameTrackBar.Value = frameTrackBar.Minimum;
+                this.Text = "Project " + selectedProject.getProjectName() + " selected, edited session = " + chosenSession.sessionName;
+            }
+
+            //Set comboBox:
+            String[] viewsList = chosenSession.getViews();
+            //MessageBox.Show(viewsList.Length + "");
+            for (int i = 0; i < viewsList.Length; i++)
+            {
+                playbackFileComboBox.Items.Add(viewsList[i]);
+            }
+
+            if (playbackFileComboBox.Items.Count > 0)
+            {
+                playbackFileComboBox.SelectedIndex = 0;
+                playbackFileComboBox.Enabled = true;
+                frameTrackBar.Enabled = true;
+                addEventAnnotationBtn.Enabled = true;
+                //pictureBox1.BackgroundImage = null;
+
+                // All toolstrips of file inside session are enables
+                toggleFileToolStripsOfSession(true);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveSessionMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode nodeS = treeView.SelectedNode;
+            //MessageBox.Show(selectedProject.getSessionN() + "");
+            if (currentSession.getEdited())
+            {
+                cleanCurrentSession();
+            }
+
+            toggleFileToolStripsOfSession(false);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void deleteSessionMenuItem_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(item.ToString());     
+            TreeNode sessionToDeleteName = treeView.SelectedNode;
+            String sName = sessionToDeleteName.Text;
+            TreeNode projectNode = sessionToDeleteName.Parent;
+            Project project = workspace.getProject(projectNode.Text);
+            if (MessageBox.Show("Confirm session removal (exclude from project): " + sName + " from " + project.getProjectName(), "Delete session", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                //1)Remove session from project:
+                project.removeSession(sName);
+                //2)Remove session from treeView:
+                treeView.BeginUpdate();
+                foreach (TreeNode currentSessionNode in selectedProjectNode.Nodes)
+                {
+                    if (currentSessionNode.Text.Equals(sName))
+                    {
+                        //MessageBox.Show("Removing " + sName + " from" + project.getProjectName());
+                        selectedProjectNode.Nodes.Remove(currentSessionNode);
+                        break;
+                    }
+                }
+                treeView.EndUpdate();
+            }
+
+            //Disable button2:
+            addEventAnnotationBtn.Enabled = false;
+            newObjectContextPanel.Visible = false;
+            clearPlaybackFileComboBox();
+            clearRightBottomPanel();
+            pictureBoard.Image = null;
+            startPoint = endPoint;
+            currentVideo = null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addFileToSessionMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                if (leftMostPanel.main.currentSession != null)
+                {
+                    String fullFileName = openFileDialog.FileName;
+                    copyFileIntoLocalSession(fullFileName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void refreshSessionMenuItem_Click(object sender, EventArgs e)
+        {
+            if (leftMostPanel.main.currentSession == null)
+            { return; }
+
+            //Check files in current Session folder
+            String[] files = Directory.GetFiles(workspace.getLocationFolder() + Path.DirectorySeparatorChar +
+                leftMostPanel.main.currentSession.getProject() + Path.DirectorySeparatorChar + leftMostPanel.main.currentSession.sessionName);
+
+            TreeNode[] arrayFiles = new TreeNode[files.Length];
+            for (int j = 0; j < arrayFiles.Length; j++)
+            {
+                arrayFiles[j] = new TreeNode(files[j].Split(Path.DirectorySeparatorChar)[files[j].Split(Path.DirectorySeparatorChar).Length - 1]);
+                arrayFiles[j].ImageIndex = 2;
+                arrayFiles[j].SelectedImageIndex = arrayFiles[j].ImageIndex;
+            }
+
+            treeView.BeginUpdate();
+            currentSessionNode.Nodes.Clear();
+            currentSessionNode.Nodes.AddRange(arrayFiles);
+            treeView.EndUpdate();
+        }
+
+    }
+}
---
+++ Annotator/object/LinkMark.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Annotator
+{
+    public class LinkMark : ObjectMark
+    {
+        public string objectId { get; }
+
+        // A set of link to other objects at a certain frame
+        // Each link is of < objectID , qualified, spatialLink >
+        public SortedSet<Tuple<string, bool, string>> binaryPredicates { get; } // By default, there is no spatial configuration attached to an object location
+
+        // A set of link to other objects of different session at a certain frame
+        // Each link is of < sessionName, objectID , qualified, spatialLink >
+        public SortedSet<Tuple<string, string, bool, string>> crossSessionBinaryPredicates { get; } // By default, there is no spatial configuration attached to an object location
+
+        public LinkMark(string objectId, int frameNo) : base(frameNo)
+        {
+            this.objectId = objectId;
+            binaryPredicates = new SortedSet<Tuple<string, bool, string>>();
+            crossSessionBinaryPredicates = new SortedSet<Tuple<string, string, bool, string>>();
+        }
+
+        public void addLinkToObject(string otherObjectId, bool qualified, string linkType)
+        {
+            binaryPredicates.Add(new Tuple<string, bool, string>(otherObjectId, qualified, linkType));
+        }
+
+        public void addLinkToObject(string sessionName, string otherObjectId, bool qualified, string linkType)
+        {
+            crossSessionBinaryPredicates.Add(new Tuple<string, string, bool, string>(sessionName, otherObjectId, qualified, linkType));
+        }
+
+        override public String ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (binaryPredicates.Count != 0)
+            {
+                sb.Append(String.Join(",", binaryPredicates.Select(u => getLiteralForm(u))));
+                if (crossSessionBinaryPredicates.Count != 0)
+                    sb.Append("\n");
+            }
+
+            if (crossSessionBinaryPredicates.Count != 0)
+                sb.Append(String.Join(",", crossSessionBinaryPredicates.Select(u => getLiteralForm(u))));
+            return sb.ToString();
+        }
+
+        public String getLiteralForm(Tuple<string, bool, string> t)
+        {
+            String q = t.Item3 + "( " + objectId + ", " + t.Item1 + " )";
+            if (!t.Item2)
+            {
+                q = "NOT( " + q + " )";
+            }
+            return q;
+        }
+
+        public String getLiteralForm(Tuple<string, string, bool, string> t)
+        {
+            String q = t.Item4 + "( " + objectId + ", " + t.Item1 + "/" + t.Item2 + ")";
+            if (!t.Item3)
+            {
+                q = "NOT( " + q + " )";
+            }
+            return q;
+        }
+
+        internal void loadFromHtml(XmlNode linkNode)
+        {
+            foreach (XmlNode linkto in linkNode.SelectNodes(Object.LINKTO))
+            {
+                try
+                {
+                    String linkToObjectId = linkto.Attributes[Object.ID].Value;
+                    bool qualified = bool.Parse(linkto.Attributes[Object.QUALIFIED].Value);
+                    string markType = linkto.Attributes[Object.TYPE].Value;
+
+                    if (linkto.Attributes[Object.OTHER_SESSION] != null)
+                    {
+                        String otherSessionName = linkto.Attributes[Object.OTHER_SESSION].Value;
+                        this.addLinkToObject(otherSessionName, linkToObjectId, qualified, markType);
+                    }
+                    else
+                    {
+                        this.addLinkToObject(linkToObjectId, qualified, markType);
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+
+        internal void writeToXml(XmlWriter xmlWriter)
+        {
+            foreach (Tuple<string, bool, string> link in binaryPredicates)
+            {
+                xmlWriter.WriteStartElement(Object.LINKTO);
+                xmlWriter.WriteAttributeString(Object.ID, link.Item1);
+                xmlWriter.WriteAttributeString(Object.QUALIFIED, "" + link.Item2);
+                xmlWriter.WriteAttributeString(Object.TYPE, link.Item3.ToString());
+                xmlWriter.WriteEndElement();
+            }
+
+            foreach (Tuple<string, string, bool, string> link in crossSessionBinaryPredicates)
+            {
+                xmlWriter.WriteStartElement(Object.LINKTO);
+                xmlWriter.WriteAttributeString(Object.OTHER_SESSION, link.Item1);
+                xmlWriter.WriteAttributeString(Object.ID, link.Item2);
+                xmlWriter.WriteAttributeString(Object.QUALIFIED, "" + link.Item3);
+                xmlWriter.WriteAttributeString(Object.TYPE, link.Item4.ToString());
+                xmlWriter.WriteEndElement();
+            }
+        }
+    }
+}
